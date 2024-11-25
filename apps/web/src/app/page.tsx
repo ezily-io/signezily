70b448d7dcf2bee655ad5972244c639c189546a1
Audrey Lin <aauddrey@gmail.com>
2024-11-25 15:37:10 +0800

    chore: add GTM

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,14 @@
+import { GoogleTagManager } from '@next/third-parties/google';
+
 import { setupI18nSSR } from '@documenso/lib/client-only/providers/i18n.server';
 
 export default async function DashboardPage() {
   await setupI18nSSR();
 
-  return <div>Documenso</div>;
+  return (
+    <>
+      <div>Documenso</div>
+      <GoogleTagManager gtmId="GTM-K2GZQVN8" />
+    </>
+  );
 }
