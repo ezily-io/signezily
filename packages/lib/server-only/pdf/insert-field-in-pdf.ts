6511146ad0a81bd39e7b37236f20612d8152fd70
Audrey Lin <aauddrey@gmail.com>
2024-11-30 14:57:50 +0800

    fix: change TC font to serve it locally

--- packages/lib/server-only/pdf/insert-field-in-pdf.ts
+++ packages/lib/server-only/pdf/insert-field-in-pdf.ts
@@ -4,7 +4,6 @@ import type { PDFDocument } from 'pdf-lib';
 import { RotationTypes, degrees, radiansToDegrees } from 'pdf-lib';
 import { P, match } from 'ts-pattern';
 
-import { PMINGLIU_FONT_PATH } from '@documenso/lib/constants/pdf';
 import {
   DEFAULT_HANDWRITING_FONT_SIZE,
   DEFAULT_STANDARD_FONT_SIZE,
@@ -30,13 +29,8 @@ export const insertFieldInPDF = async (pdf: PDFDocument, field: FieldWithSignatu
   const fontCaveat = await fetch(process.env.FONT_CAVEAT_URI).then(async (res) =>
     res.arrayBuffer(),
   );
+  const fontPming = await fetch(process.env.FONT_PMING_URI).then(async (res) => res.arrayBuffer());
 
-  console.log(`Using pming- ${PMINGLIU_FONT_PATH()}`);
-  const fontPming = await fetch(PMINGLIU_FONT_PATH()).then(async (res) => {
-    return res.arrayBuffer();
-  });
-
-  console.log(PMINGLIU_FONT_PATH());
   const isSignatureField = isSignatureFieldType(field.type);
 
   pdf.registerFontkit(fontkit);
