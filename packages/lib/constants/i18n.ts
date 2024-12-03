5f7ec508e102a843bc0ef5a40cb0e72fed47e91b
Audrey Lin <aauddrey@gmail.com>
2024-12-03 11:26:00 +0800

    fix: default locale to zh-tw

--- packages/lib/constants/i18n.ts
+++ packages/lib/constants/i18n.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod';
 
-export const SUPPORTED_LANGUAGE_CODES = ['de', 'en', 'fr', 'es', 'it', 'pl', 'zh_tw'] as const;
+export const SUPPORTED_LANGUAGE_CODES = ['de', 'en', 'fr', 'es', 'it', 'pl', 'zh-tw'] as const;
 
-export const ZSupportedLanguageCodeSchema = z.enum(SUPPORTED_LANGUAGE_CODES).catch('en');
+export const ZSupportedLanguageCodeSchema = z.enum(SUPPORTED_LANGUAGE_CODES).catch('zh-tw');
 
 export type SupportedLanguageCodes = (typeof SUPPORTED_LANGUAGE_CODES)[number];
 
@@ -20,8 +20,8 @@ export type I18nLocaleData = {
 
 export const APP_I18N_OPTIONS = {
   supportedLangs: SUPPORTED_LANGUAGE_CODES,
-  sourceLang: 'en',
-  defaultLocale: 'en-US',
+  sourceLang: 'zh-tw',
+  defaultLocale: 'zh-tw',
 } as const;
 
 type SupportedLanguage = {
@@ -54,9 +54,9 @@ export const SUPPORTED_LANGUAGES: Record<string, SupportedLanguage> = {
     short: 'pl',
     full: 'Polish',
   },
-  zh_tw: {
+  ['zh-tw']: {
     full: '繁體中文',
-    short: 'zh_tw',
+    short: 'zh-tw',
   },
 } satisfies Record<SupportedLanguageCodes, SupportedLanguage>;
 
