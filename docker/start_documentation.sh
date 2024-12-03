fcda95fc46986003c970775c1fc32ffee4fec5aa
Giancarlo Mora Arias <giank.ma@gmail.com>
2024-12-03 11:30:43 -0600

    everride port

--- docker/start_documentation.sh
+++ docker/start_documentation.sh
@@ -2,4 +2,11 @@
 
 set -x
 
+# Export the PORT environment variable
+export PORT=${PORT:-3002} # Default to 3001 if PORT is not set
+
+echo "PORT is set to $PORT"
+
+npx prisma migrate deploy --schema ./packages/prisma/schema.prisma
+
 node apps/documentation/server.js
--- docker/start_marketing.sh
+++ docker/start_marketing.sh
@@ -2,5 +2,11 @@
 
 set -x
 
+# Export the PORT environment variable
+export PORT=${PORT:-3001} # Default to 3001 if PORT is not set
+
+echo "PORT is set to $PORT"
+
+npx prisma migrate deploy --schema ./packages/prisma/schema.prisma
 
 node apps/marketing/server.js
