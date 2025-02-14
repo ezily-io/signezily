aa17e1e41a2d1a00e694cb9fd023414eed672247
Mythie <me@lucasjamessmith.me>
2025-02-14 14:53:01 +1100

    chore: make all the docker stuff work

--- docker/start.sh
+++ docker/start.sh
@@ -6,33 +6,6 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-value=$1
+npx prisma migrate deploy --schema ../../packages/prisma/schema.prisma
 
-# Use a case statement to handle different values
-case $value in
-  marketing)
-    echo "Running marketing site"
-    sh generate_cert_p12.sh
-    set -x
-    npx prisma migrate deploy --schema ./packages/prisma/schema.prisma
-    node apps/marketing/server.js
-    ;;
-  default)
-    echo "Running documenso main website"
-    sh generate_cert_p12.sh
-    set -x
-    npx prisma migrate deploy --schema ./packages/prisma/schema.prisma
-    node apps/web/server.js
-    ;;
-  documentation)
-    echo "Running documentation site"
-    sh generate_cert_p12.sh
-    set -x
-    npx prisma migrate deploy --schema ./packages/prisma/schema.prisma
-    node apps/documentation/server.js
-    ;;
-  *)
-    echo "No valid argument provided. Exiting."
-    exit 1
-    ;;
-esac
+HOSTNAME=0.0.0.0 node build/server/main.js
