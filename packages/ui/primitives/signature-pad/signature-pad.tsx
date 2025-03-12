7e8e98673c978df0417d7ed7b2623f6546d18307
Ephraim Duncan <55143799+ephraimduncan@users.noreply.github.com>
2025-03-12 05:43:52 +0000

    fix: signing field disabled when pointer is out of canvas (#1652)

--- packages/ui/primitives/signature-pad/signature-pad.tsx
+++ packages/ui/primitives/signature-pad/signature-pad.tsx
@@ -272,7 +272,11 @@ export const SignaturePad = ({
       event.preventDefault();
     }
 
-    onMouseUp(event, false);
+    if (isPressed) {
+      onMouseUp(event, true);
+    } else {
+      onMouseUp(event, false);
+    }
   };
 
   const onClearClick = useCallback(() => {
