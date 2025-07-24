52c9e2e86f7e3513999196abb27103a5e5848553
David Nguyen <davidngu28@gmail.com>
2025-07-24 16:05:00 +1000

    feat: add email domains (#1895)
    Implemented Email Domains which allows Platform/Enterprise customers to
    send emails to recipients using their custom emails.

--- packages/lib/constants/email.ts
+++ packages/lib/constants/email.ts
@@ -1,10 +1,15 @@
 import { env } from '../utils/env';
 
-export const FROM_ADDRESS = env('NEXT_PRIVATE_SMTP_FROM_ADDRESS') || 'noreply@documenso.com';
-export const FROM_NAME = env('NEXT_PRIVATE_SMTP_FROM_NAME') || 'Documenso';
+export const FROM_ADDRESS = env('NEXT_PRIVATE_SMTP_FROM_ADDRESS') || 'noreply@ezily.io';
+export const FROM_NAME = env('NEXT_PRIVATE_SMTP_FROM_NAME') || 'SignEzily';
+
+export const SUPPORT_EMAIL = process.env.NEXT_PRIVATE_SUPPORT_EMAIL || 'support@ezily.io';
+export const DOCUMENSO_INTERNAL_EMAIL = {
+  name: FROM_NAME,
+  address: FROM_ADDRESS,
+};
 
 export const SERVICE_USER_EMAIL = 'support@ezily.io';
-export const SUPPORT_EMAIL = process.env.NEXT_PRIVATE_SUPPORT_EMAIL || 'support@ezily.io';
 
 export const EMAIL_VERIFICATION_STATE = {
   NOT_FOUND: 'NOT_FOUND',
