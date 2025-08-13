870d0d2820e29a84ef4131b5199b9448908b2e0f
David Diaz <seich@martianwabbit.com>
2025-08-13 23:57:31 +0200

    fix: updated nodejs versions to match new requirements

--- docker/start.sh
+++ docker/start.sh
@@ -6,6 +6,33 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-npx prisma migrate deploy --schema ../../packages/prisma/schema.prisma
+value=$1
 
-HOSTNAME=0.0.0.0 node build/server/main.js
+# Use a case statement to handle different values
+case $value in
+  marketing)
+    echo "Running marketing site"
+    sh generate_cert_p12.sh
+    set -x
+    npx prisma migrate deploy --schema ./packages/prisma/schema.prisma
+    node apps/marketing/server.js
+    ;;
+  default)
+    echo "Running documenso main website"
+    sh generate_cert_p12.sh
+    set -x
+    npx prisma migrate deploy --schema ./packages/prisma/schema.prisma
+    node apps/web/server.js
+    ;;
+  documentation)
+    echo "Running documentation site"
+    sh generate_cert_p12.sh
+    set -x
+    npx prisma migrate deploy --schema ./packages/prisma/schema.prisma
+    node apps/documentation/server.js
+    ;;
+  *)
+    echo "No valid argument provided. Exiting."
+    exit 1
+    ;;
+esac
